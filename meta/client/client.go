85a2889527d5393fefa5c6b947080215125543bd
cuixiaotian <cuixiaotian@unicloud.com>
2020-04-17 23:01:20 +0800

    finish interface function completion

--- meta/client/client.go
+++ meta/client/client.go
@@ -13,6 +13,7 @@ type Client interface {
 	NewTrans() (tx Tx, err error)
 	AbortTrans(tx Tx) error
 	CommitTrans(tx Tx) error
+
 	//object
 	GetObject(bucketName, objectName, version string) (object *Object, err error)
 	GetLatestObjectVersion(bucketName, objectName string) (object *Object, err error)
@@ -23,9 +24,7 @@ type Client interface {
 	UpdateAppendObject(object *Object) error
 	MigrateObject(object *Object) error
 	RenameObject(object *Object, sourceObject string) (err error)
-
 	ReplaceObjectMetas(object *Object, tx Tx) (err error)
-
 	DeleteObject(object *Object, tx Tx) error
 	DeleteObjectPart(object *Object, tx Tx) error
 	UpdateObjectAcl(object *Object) error
@@ -36,12 +35,10 @@ type Client interface {
 	GetBuckets() (buckets []Bucket, err error)
 	PutBucket(bucket Bucket) error
 	PutNewBucket(bucket Bucket) error
-	CheckAndPutBucket(bucket Bucket) (bool, error)
 	DeleteBucket(bucket Bucket) error
 	ListObjects(bucketName, marker, prefix, delimiter string, maxKeys int) (listInfo ListObjectsInfo, err error)
 	ListLatestObjects(bucketName, marker, prefix, delimiter string, maxKeys int) (listInfo ListObjectsInfo, err error)
 	ListVersionedObjects(bucketName, marker, verIdMarker, prefix, delimiter string, maxKeys int) (listInfo VersionedListObjectsInfo, err error)
-
 	UpdateUsage(bucketName string, size int64, tx Tx) error
 	IsEmptyBucket(bucket *Bucket) (isEmpty bool, err error)
 
@@ -50,23 +47,26 @@ type Client interface {
 	CreateMultipart(multipart Multipart) (err error)
 	PutObjectPart(multipart *Multipart, part *Part) (err error)
 	DeleteMultipart(multipart *Multipart, tx Tx) (err error)
-
 	ListMultipartUploads(bucketName, keyMarker, uploadIdMarker, prefix, delimiter, encodingType string, maxUploads int) (result datatype.ListMultipartUploadsResponse, err error)
+
 	//cluster
 	GetClusters() (cluster []Cluster, err error)
+
 	//lc
 	PutBucketToLifeCycle(lifeCycle LifeCycle) error
 	GetBucketLifeCycle(bucket Bucket) (lifeCycle *LifeCycle, err error)
 	RemoveBucketFromLifeCycle(bucket Bucket) error
 	ScanLifeCycle(limit int, marker string) (result ScanLifeCycleResult, err error)
+
 	//user
 	GetUserBuckets(userId string) (buckets []string, err error)
-	RemoveBucketForUser(bucketName string, userId string) (err error)
+
 	//gc
 	PutObjectToGarbageCollection(object *Object, tx Tx) error
 	ScanGarbageCollection(limit int) ([]GarbageCollection, error)
 	PutFreezerToGarbageCollection(object *Freezer, tx Tx) (err error)
 	RemoveGarbageCollection(garbage GarbageCollection) error
+
 	//freezer
 	CreateFreezer(freezer *Freezer) (err error)
 	GetFreezer(bucketName, objectName, version string) (freezer *Freezer, err error)
